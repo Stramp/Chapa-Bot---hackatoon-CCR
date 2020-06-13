40f7e1ecf2f589d538db57a42a6a54b2e68fa3d3
Stramp <rafaelcostart2@gmail.com>
2020-06-13 12:02:10 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const wa = require('@open-wa/wa-automate');
+
+wa.create().then(client => start(client));
+
+async function start(client) {
+    await client.onAnyMessage(message => {
+        console.log("body >>>>", message.body);
+        console.log("mediaData >>>>", message.mediaData);
+        console.log("fromMe >>>> ", message.fromMe);
+        return message.body === "falarafael" ?
+            client.sendText(message.from, '👋 Olá! eu sou um BOT otario') : null
+    })
+}
